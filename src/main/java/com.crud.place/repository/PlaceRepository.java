fbb7c997c96f43cc4ea9b5b4c1f3bf98bebeaa0f
julytatiana <yulytatiana@live.com>
2022-04-28 10:00:16 -0500

    PlaceRepository was updated

--- src/main/java/com.crud.place/repository/PlaceRepository.java
+++ src/main/java/com.crud.place/repository/PlaceRepository.java
@@ -1,6 +1,5 @@
 package com.crud.place.repository;
 
-import com.crud.crudplace.entity.Book;
 import com.crud.place.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
